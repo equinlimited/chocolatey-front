82793b5784c259ee2dba6a04a8bd514886612cea
Mike Cooper <mike@equin.co.uk>
2026-04-08 19:13:51 +0100

    Use machine wide installer

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = $env:ChocolateyPackageName
 
-$url = "https://dl.frontapp.com/desktop/builds/$($env:ChocolateyPackageVersion)/Front-$($env:ChocolateyPackageVersion)-x64.msi"
+$url = "https://dl.frontapp.com/desktop/builds/3.71.1/Front-machine-3.71.1-x64.msi"
 
 Write-Host "NOTE: This is an UNOFFICIAL, community-maintained Chocolatey package for Front."
 Write-Host "It is not maintained or endorsed by Front and may lag behind official releases."
@@ -14,10 +14,10 @@ $packageArgs = @{
 
   softwareName  = 'Front'
 
-  checksum      = '1093E6E06D5CE149C2965822D01CDA9DE236CE45340B9B835CFD9C13900812D9'
+  checksum      = '1EF4B2C21C713C4B4342A214624E3EECD136CDDDA95BFCC30376C388674351DA'
   checksumType  = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:ChocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).3.71.1.MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
