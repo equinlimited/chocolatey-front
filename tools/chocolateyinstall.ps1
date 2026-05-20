62755d7f3a87dcf71813c35a1d327fd95072e7db
Mike Cooper <mike@equin.co.uk>
2026-05-20 15:31:10 +0100

    v3.73.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = $env:ChocolateyPackageName
 
-$url = "https://dl.frontapp.com/desktop/builds/3.71.1/Front-machine-3.71.1-x64.msi"
+$url = "https://dl.frontapp.com/desktop/builds/3.73.0/Front-machine-3.73.0-x64.msi"
 
 Write-Host "NOTE: This is an UNOFFICIAL, community-maintained Chocolatey package for Front."
 Write-Host "It is not maintained or endorsed by Front and may lag behind official releases."
@@ -14,10 +14,10 @@ $packageArgs = @{
 
   softwareName  = 'Front'
 
-  checksum      = '1EF4B2C21C713C4B4342A214624E3EECD136CDDDA95BFCC30376C388674351DA'
+  checksum      = '4390C6DC667C2C37C5565255A03819172DAA29D6A775A6296E3B4FCA5D75543B'
   checksumType  = 'sha256'
 
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).3.71.1.MsiInstall.log`""
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).3.73.0.MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
